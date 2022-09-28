2b6ff5f513441a63df26728b667992c4394a5078
Daniel Saraiva <danieldsvn@gmail.com>
2022-09-28 12:16:44 -0300

    Implementados atributos e respectivos metodos get na classe Character. Falta implementar métodos attack e special

--- src/Character.ts
+++ src/Character.ts
@@ -4,22 +4,31 @@ import Fighter from './Fighter';
 import Race, { Elf } from './Races';
 
 class Character implements Fighter {
-  private race: Race;
-  private archetype: Archetype;
-  private maxLifePoints: number;
-  private lifePoints: number;
-  private strength: number;
-  private defense: number;
-  private dexterity: number;
-  private energy: Energy;
-  private name: string;
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+  private _name: string;
   constructor(name: string) {    
-    this.name = name;    
-    this.dexterity = Math.floor(Math.random() * (11 - 1) + 1);    
-    this.race = new Elf(name, this.dexterity);
-    this.archetype = new Mage(name);
-    this.maxLifePoints =
+    const randomNumber = Math.floor(Math.random() * (11 - 1) + 1);
+    this._name = name;    
+    this._dexterity = randomNumber;    
+    this._race = new Elf(name, this._dexterity);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this.race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = randomNumber;
+    this._defense = randomNumber;
+    this._energy = { type_: this._archetype.energyType, amount: randomNumber };
   }
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?: Energy | undefined;
   attack(enemy: Fighter): void {
     throw new Error('Method not implemented.');
   }
@@ -33,4 +42,7 @@ class Character implements Fighter {
     throw new Error('Method not implemented.');
   }
   
+  get race(): Race {
+    return this._race;
+  }
 }
